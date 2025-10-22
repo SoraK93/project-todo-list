a356cad21cbf150672411400877b72bcb5616b35
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-22 20:45:40 +0530

    Update: create project and show todolist

--- src/js/project.js
+++ src/js/project.js
@@ -1,10 +1,31 @@
-const project = function() {
-    const toDoList = []
-    // TODO: function create todoList
-        // TODO: create get functions for each
+import { Todo } from "./todo";
 
-    // TODO: function review todoList
-    // TODO: function update todoList
-    // TODO: function delete todoList
-}
+export const project = function () {
+  const toDoList = [];
+  // TODO: function create todoList
+  // TODO: create get functions for each
+  /** Takes a list of todos and create a new project */
+  const createNewProject = function (todoObject) {
+    let todo = new Todo()
+    todo.title = todoObject.title
+    toDoList.push(todo)
+  };
 
+  // TODO: function review todoList
+  const reviewProject = function () {
+    return toDoList
+  };
+
+  // TODO: function update todoList
+  const updateProject = function () {};
+
+  // TODO: function delete todoList
+  const deleteProject = function () {};
+
+  return {
+    createNewProject,
+    reviewProject,
+    updateProject,
+    deleteProject,
+  };
+};
