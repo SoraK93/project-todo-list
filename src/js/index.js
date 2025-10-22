824b78c24bb78925cf836bc62ceeefa9fd157b39
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-22 20:48:45 +0530

    Modify: Minor changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 import "../css/style.css";
 import "../template.html";
+import { project } from "./project";
+import { createProjectForm } from "./createPageElements";
 
-console.log("It's runing");
+const newProject = project();
+newProject.createNewProject({ title: "Lets Todo" });
+
+const newProject2 = project();
+newProject2.createNewProject({ title: "Lets Todo 2" });
+
+document.querySelector(".project").appendChild(createProjectForm)
+
+console.log(newProject.reviewProject()[0].title);
+console.log(newProject2.reviewProject()[0].title);
+
